be528dc608eaec064987b4b1ab3d7be74b6abb33
Alysson Barrera <alyssonbarrera@outlook.com>
2022-10-29 14:58:56 -0300

    :sparkles: implementation of new features

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,44 @@
-import { Text, View } from 'react-native';
+import { Button } from '@components/Button';
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { Container} from './styles';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <Header />
+
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={ groups.length === 0 && { flex: 1 } }
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        )}
+      />
+
+      <Button
+        title='Cadastrar turma'
+      />
+
+    </Container>
   );
-}
+};
